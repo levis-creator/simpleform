822c3b43d2eceb43ac1846be0f356eccfb1bcbd7
levis-creator <47003569+levis-creator@users.noreply.github.com>
2023-11-22 04:13:35 +0300

    v1.0.1

--- app/src/main/java/com/macrosoft/simpleform/MainActivity.java
+++ app/src/main/java/com/macrosoft/simpleform/MainActivity.java
@@ -1,6 +1,9 @@
 package com.macrosoft.simpleform;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.firstname), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void  onSubmit(View view){
+//        fetches items from the interface.
+        EditText firstname=findViewById(R.id.firstname);
+        EditText lastname=findViewById(R.id.lastname);
+        EditText email=findViewById(R.id.emailAddress);
+//        textview
+        TextView firstnameTxt=findViewById(R.id.txtFirstName);
+        TextView lastnameTxt=findViewById(R.id.txtLastName);
+        TextView emailTxt=findViewById(R.id.txtEmail);
+
+//        fetching inputted data from the input and setting text data
+        firstnameTxt.setText("Firstname "  + firstname.getText().toString());
+        lastnameTxt.setText("Lastname " + lastname.getText().toString());
+        emailTxt.setText("Email " + email.getText().toString());
+
     }
 }
